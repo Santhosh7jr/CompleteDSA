a66cd12caa4cf29f099f273a4ad581a788c44d9a
Santhosh J R <137148886+Santhosh7jr@users.noreply.github.com>
2025-08-14 08:13:19 +0530

    Add files via upload

---
+++ DataStructures/Strings/StringBuffers.java
@@ -0,0 +1,61 @@
+package DataStructures.Strings;
+import java.util.*;
+
+public class StringBuffers {
+
+    public static void main(String[] args) {
+
+        /*StringBuffer sb = new StringBuffer();
+        sb.append("hello world!");
+
+        String str = sb.toString();
+
+        System.out.println(sb);
+        System.out.println(str);*/
+
+
+        /*StringBuffer sb2 = new StringBuffer("This is string buffer");
+        System.out.println(sb2);
+
+        sb2.append(" class");
+        System.out.println(sb2);
+
+        sb2.insert(1,"tttt");
+        System.out.println(sb2);
+
+        sb2.replace(1,5,"");
+        System.out.println(sb2);*/
+
+
+//        System.out.println(generateRandomStr());
+
+
+        /*String sentence ="    h   e   l  l  o    !";
+
+        System.out.println(sentence);
+        System.out.println(sentence.replaceAll("\\s",""));*/
+
+
+        String arr = "this is an array";
+        System.out.println(Arrays.toString(arr.split(" ")));
+
+        System.out.println(" ಶುಭಾಶಯಗಳು ಮತ್ತು ಸಭ್ಯ ಅಭಿವ್ಯಕ್ತಿಗಳು · 2. ದೈನಂದಿನ ಸಂಭಾಷಣೆ ನುಡಿಗಟ್ಟುಗಳು · 3. ಪಟ್ಟಣವನ್ನು ಸುತ್ತುವುದು · 4. ಆಹಾರ ಮತ್ತು ಊಟದ ನುಡಿಗಟ್ಟುಗಳು");
+
+
+    }
+
+    private static String generateRandomStr(){
+
+        StringBuffer sb = new StringBuffer();
+        Random random = new Random();
+
+        for(int i=0;i<5;i++){
+            int num=65+(int) (26*random.nextFloat());
+            sb.append((char) (num));
+        }
+
+        return sb.toString();
+
+    }
+
+}
---
+++ DataStructures/Strings/Strings.java
@@ -0,0 +1,11 @@
+package DataStructures.Strings;
+
+public class Strings{
+    //declaration
+    String str;
+
+    public Strings(String str){
+        this.str=str;
+    }
+
+}
