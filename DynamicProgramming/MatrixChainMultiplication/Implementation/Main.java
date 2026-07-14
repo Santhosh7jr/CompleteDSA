8d77fd079b18d1493cb88b4e10099748ea054997
Santhosh J R <137148886+Santhosh7jr@users.noreply.github.com>
2026-07-14 12:29:25 +0530

    Add Matrix Chain Multiplication implementation
    Implemented the Matrix Chain Multiplication algorithm to calculate the minimum number of multiplications required for matrix multiplication.

--- DynamicProgramming/MatrixChainMultiplication/Implementation/Main.java
+++ DynamicProgramming/MatrixChainMultiplication/Implementation/Main.java
@@ -5,7 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] matrixDimensions = {40, 20, 30, 10, 30};
 
+        System.out.println(getMinimumNumberOfMultiplicationsRequired(matrixDimensions, 1, matrixDimensions.length - 1));
+
+    }
+
+    public static int getMinimumNumberOfMultiplicationsRequired(int[] arr, int left, int right) {
+
+        if (left >= right) return 0;
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = left; i < right; i++) {
+            min = Math.min(min,
+                    (
+                        getMinimumNumberOfMultiplicationsRequired(arr, left, i) +
+                        getMinimumNumberOfMultiplicationsRequired(arr, i + 1, right) +
+                        (arr[left - 1] * arr[i] * arr[right])
+                    ));
+        }
+
+        return min;
 
     }
 
